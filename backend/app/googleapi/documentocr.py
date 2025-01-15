0119a5621df5b015beb8d77f6334830a658a7bb7
Pradeep Sahu <sahupra1357@gmail.com>
2025-01-15 00:03:20 -0500

    developmet_extractor_table_1

---
+++ backend/app/googleapi/documentocr.py
@@ -0,0 +1,91 @@
+from typing import Optional
+
+from google.api_core.client_options import ClientOptions # type: ignore
+#from google.auth.credentials import Credentials # type: ignore
+from google.cloud import documentai  # type: ignore
+
+# TODO(developer): Uncomment these variables before running the sample.
+project_id = "strategic-guru-444919-s3"
+location = "us" # Format is "us" or "eu"
+processor_id = "9728391baa213d76" # Create processor before running sample
+#file_path = "/home/mxmanu1234/100149908178-095737680706-.PDF"
+mime_type = "application/pdf" # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
+field_mask = "text,entities,pages.pageNumber"  # Optional. The fields to return in the Document object.
+# processor_version_id = "YOUR_PROCESSOR_VERSION_ID" # Optional. Processor version to use
+
+
+def process_document_sample(
+    project_id: str,
+    location: str,
+    processor_id: str,
+    file_path: str,
+    mime_type: str,
+    field_mask: Optional[str] = None,
+    processor_version_id: Optional[str] = None,
+) -> None:
+    print("The document uploaded successfully and about to call the process_document_sample")
+    # You must set the `api_endpoint` if you use a location other than "us".
+#     cred = Credentials({
+#   "account": "",
+#   "client_id": "764086051850-6qr4p6gpi6hn506pt8ejuq83di341hur.apps.googleusercontent.com",
+#   "client_secret": "d-FL95Q19q7MQmFpd7hHD0Ty",
+#   "quota_project_id": "strategic-guru-444919-s3",
+#   "refresh_token": "1//05Ms3HJeAfzTaCgYIARAAGAUSNwF-L9IrmF0MaVFpQjwDOuO1u8Wr0bS6VJFNlkZzMhRM9zT-y9IGMGeOHq3ptoswkQuAwtQBzns",
+#   "type": "authorized_user",
+#   "universe_domain": "googleapis.com"
+# })
+    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
+    client = documentai.DocumentProcessorServiceClient(client_options=opts)
+
+    if processor_version_id:
+        # The full resource name of the processor version, e.g.:
+        # `projects/{project_id}/locations/{location}/processors/{processor_id}/processorVersions/{processor_version_id}`
+        name = client.processor_version_path(
+            project_id, location, processor_id, processor_version_id
+        )
+    else:
+        # The full resource name of the processor, e.g.:
+        # `projects/{project_id}/locations/{location}/processors/{processor_id}`
+        name = client.processor_path(project_id, location, processor_id)
+
+    # Read the file into memory
+    with open(file_path, "rb") as image:
+        image_content = image.read()
+
+    # Load binary data
+    raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
+
+    # For more information: https://cloud.google.com/document-ai/docs/reference/rest/v1/ProcessOptions
+    # Optional: Additional configurations for processing.
+    process_options = documentai.ProcessOptions(
+        # Process only specific pages
+        individual_page_selector=documentai.ProcessOptions.IndividualPageSelector(
+            pages=[1]
+        )
+    )
+
+    # Configure the process request
+    request = documentai.ProcessRequest(
+        name=name,
+        raw_document=raw_document,
+        field_mask=field_mask,
+        process_options=process_options,
+    )
+
+    result = client.process_document(request=request)
+
+    # For a full list of `Document` object attributes, reference this page:
+    # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
+    document = result.document
+
+    # Read the text recognition output from the processor
+    print("The document contains the following text:")
+    print(document.text)
+
+    return document
+
+
+#process_document_sample(project_id,location,processor_id,file_path,mime_type,field_mask)
+
+def process_document(file_path: str):
+    return process_document_sample(project_id,location,processor_id,file_path,mime_type,field_mask)
